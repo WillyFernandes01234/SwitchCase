9642d37f66ec5e74d4a50b58142005aa45a81bce
WillyFernandes01234 <99400093+WillyFernandes01234@users.noreply.github.com>
2022-03-31 19:13:29 -0300

    SwitchCase
    commit

---
+++ script.js
@@ -0,0 +1,156 @@
+function executarAcao1(){
+    let valor = 1;
+    switch(valor){
+        case 1: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha0");
+            div1.classList.add("filha2");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            let div5 = document.getElementById("msg5");
+            div5.classList.remove("filha2");
+            div5.classList.add("filha0");
+            let div6 = document.getElementById("msg6");
+            div6.classList.remove("filha2");
+            div6.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao2(){
+    let valor = 2;
+    switch(valor){
+        case 2: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha0");
+            div2.classList.add("filha2");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            let div5 = document.getElementById("msg5");
+            div5.classList.remove("filha2");
+            div5.classList.add("filha0");
+            let div6 = document.getElementById("msg6");
+            div6.classList.remove("filha2");
+            div6.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao3(){
+    let valor = 3;
+    switch(valor){
+        case 3: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha0");
+            div3.classList.add("filha2");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            let div5 = document.getElementById("msg5");
+            div5.classList.remove("filha2");
+            div5.classList.add("filha0");
+            let div6 = document.getElementById("msg6");
+            div6.classList.remove("filha2");
+            div6.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao4(){
+    let valor = 4;
+    switch(valor){
+        case 4: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha0");
+            div4.classList.add("filha2");
+            let div5 = document.getElementById("msg5");
+            div5.classList.remove("filha2");
+            div5.classList.add("filha0");
+            let div6 = document.getElementById("msg6");
+            div6.classList.remove("filha2");
+            div6.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao5(){
+    let valor = 5;
+    switch(valor){
+        case 5: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            let div5 = document.getElementById("msg5");
+            div5.classList.remove("filha0");
+            div5.classList.add("filha2");
+            let div6 = document.getElementById("msg6");
+            div6.classList.remove("filha2");
+            div6.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao6(){
+    let valor = 6;
+    switch(valor){
+        case 6: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            let div5 = document.getElementById("msg5");
+            div5.classList.remove("filha2");
+            div5.classList.add("filha0");
+            let div6 = document.getElementById("msg6");
+            div6.classList.remove("filha0");
+            div6.classList.add("filha2");
+            break;
+        }
+    }
+}
